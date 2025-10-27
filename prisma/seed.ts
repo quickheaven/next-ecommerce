a3844bbbf8b36f84bad75c40357845e8a7a5f1bf
ACristobal <ajie.io@yahoo.com>
2025-10-26 20:53:59 -0400

    25 Seeding: Prisma Client, Deleting Data && Inserting Categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,44 @@
-console.log('Seeding database...');
+import { config } from 'dotenv';
+import { PrismaClient } from "../app/generated/prisma/client";
+
+// Load environment variables
+config();
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMany();
+
+    const electronics = await prisma.category.create({
+        data: {
+            name: "Electronics",
+            slug: "electronics"
+        }
+    });
+
+    const clothing = await prisma.category.create({
+        data: {
+            name: "Clothing",
+            slug: "clothing"
+        }
+    });
+
+    const home = await prisma.category.create({
+        data: {
+            name: "Home",
+            slug: "home"
+        }
+    });
+}   
+
+main()
+  .then(async () => {
+    console.log("Seeding complete!");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
