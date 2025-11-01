1643903caf02e2b887f245f17728b80f4f9cb547
ACristobal <ajie.io@yahoo.com>
2025-10-31 23:20:06 -0400

    28 Fetching Products in Server Component

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -1,19 +1,22 @@
-import { Product } from "@/lib/mocks";
+
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
+import { Product } from "../generated/prisma/client";
 
 
 export function ProductCard({ product }: { product: Product}) {
     return (
         <div className="border border-gray-200 rounded-lg p-4">
             <div className="relative aspect-video">
-                <Image
-                    src={`${product.image}`}
-                    alt={product.name}
-                    fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                    className="object-cover"
-                />
+                {product.image && (
+                    <Image
+                        src={`${product.image}`}
+                        alt={product.name}
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        className="object-cover"
+                    />
+                )}
             </div>
             <h2 className="text-lg font-semibold">{product.name}</h2>
             <p className="text-gray-600">{formatPrice(product.price)}</p>
