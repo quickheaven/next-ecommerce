171e6ff86dceb7f8d00501892e1b37037ee24e14
ACristobal <ajie.io@yahoo.com>
2025-11-14 14:50:51 -0500

    Cleanup, removed commented code.

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,6 @@ import { ProductCardSkeleton } from "./ProductCardSkeleton";
 
 export default function Loading() {
   return (
-    // <div className="flex items-center justify-center h-screen">
-    //   <div className="w-20 h-20 border-t-2 border-b-2 border-gray-800 rounded-full animate-spin"></div>
-    // </div>
     <main className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Home</h1>
       <p>Showing 5 products</p>
