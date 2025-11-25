21683e59c539a9eeb759f9d9d98b1a4a124bc0d7
ACristobal <ajie.io@yahoo.com>
2025-11-24 21:59:05 -0500

    42 Dynamic Routes - Product Page

---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { getProductBySlug } from "@/lib/actions";
+import { formatPrice } from '../../../lib/utils';
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params;
+  const product = await getProductBySlug(slug);
+
+  if (!product) {
+    notFound();
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">{product.name}</h1>
+      <p>{product.description}</p>
+      <p>${formatPrice(product.price)}</p>
+    </div>
+  );
+}
