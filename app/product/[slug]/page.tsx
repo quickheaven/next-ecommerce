964392745a3f1ab62831e7a823f891ef0a22a795
ACristobal <ajie.io@yahoo.com>
2025-12-01 23:47:04 -0500

    47 Product Page with Images

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@radix-ui/react-separator";
+import Image from "next/image";
 
 export async function generateMetadata({
   params,
@@ -45,17 +46,37 @@ export default async function ProductPage({ params }: { params: Promise<{ slug: 
   return (
     <main className="container mx-auto p-4">
       <Card className="max-w-3xl mx-auto">
-        <CardContent className="p-6">
+        <CardContent className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="relative rounded-lg overflow-hidden ">
+            {product.image && (
+              <Image
+                src={product.image}
+                alt={product.name}
+                fill
+                priority
+                sizes="(max-width: 768px) 100vw, 50vw"
+                className="object-cover"
+              />
+            )}
+          </div>
+          <div>
             <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+
             <div className="flex items-center gap-2 mb-4">
-              <span className="font-semibold text-lg">{formatPrice(product.price)}</span>
+              <span className="font-semibold text-lg">
+                {formatPrice(product.price)}
+              </span>
+
               <Badge variant="outline">{product.category?.name}</Badge>
             </div>
+
             <Separator className="my-4" />
+
             <div className="space-y-2">
               <h2 className="font-medium">Description</h2>
               <p>{product.description}</p>
             </div>
+          </div>
         </CardContent>
       </Card>
     </main>
