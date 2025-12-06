215f23877c70661018e32a05f139a5d796068e20
ACristobal <ajie.io@yahoo.com>
2025-12-06 16:31:42 -0500

    53 Implementing Theme Toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/theme-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({
             <ul>
               <li><Link href="//">Home</Link></li>
               <li><Link href="/products">Products</Link></li>
+              <li><ModeToggle /></li>
             </ul>
             {children}
           </>
