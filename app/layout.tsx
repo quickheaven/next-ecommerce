a938b089a5865508bcf1d5081788d47e24b8f0cc
ACristobal <ajie.io@yahoo.com>
2025-12-07 13:58:19 -0500

    Fixed build error.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/theme-toggle";
 import { Navbar } from "@/components/navbar";
 
 const geistSans = Geist({
