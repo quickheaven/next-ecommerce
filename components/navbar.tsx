3ef74a2d14af5253766a8425900cf20bcf097da4
ACristobal <ajie.io@yahoo.com>
2025-12-08 21:57:46 -0500

    55 Adding Icons to Navigation Bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import { Button } from "./ui/button";
+import { Search, ShoppingCart } from "lucide-react";
+import { ModeToggle } from "./mode-toggle";
 
 const categories = [
   { id: 1, name: "Electronics", href: "/category/electronics" },
@@ -28,7 +31,21 @@ export function Navbar() {
             </nav>
           </div>
         </div>
-        <div>Right</div>
+        <div className="flex items-center gap-4">
+          <Button variant="ghost" size="icon" asChild>
+            <Link href="/search">
+              <Search className="h-5 w-5" />
+            </Link>
+          </Button>
+
+          <Button variant="ghost" size="icon" asChild>
+            <Link href="/cart">
+              <ShoppingCart className="h-5 w-5" />
+            </Link>
+          </Button>
+
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
