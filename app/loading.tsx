e673bcca5225cfd55ac124d8427418df7853bd3e
ACristobal <ajie.io@yahoo.com>
2025-12-19 22:07:22 -0500

    61 Fixing Layout Issues & Adding Breadcrumbs

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,11 @@
+
+import { BreadcrumbsSkeleton } from "@/components/breadcrumbs-skeleton";
 import ProductsSkeleton from "./ProductsSkeleton";
 
 export default function Loading() {
   return (
     <main className="container mx-auto p-4">
+      <BreadcrumbsSkeleton />
       <ProductsSkeleton />
     </main>
   );
--- app/product/[slug]/loading.tsx
+++ app/product/[slug]/loading.tsx
@@ -1,3 +1,4 @@
+import { BreadcrumbsSkeleton } from "@/components/breadcrumbs-skeleton";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -5,7 +6,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default async function Loading() {
   return (
     <main className="container mx-auto p-4">
-      <Card className="max-w-3xl mx-auto ">
+      <BreadcrumbsSkeleton />
+      <Card>
         <CardContent className="p-6 space-y-4">
           <Skeleton className="h-10 w-3/4" />
 
