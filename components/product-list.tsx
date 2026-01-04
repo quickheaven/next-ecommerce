b1168ac74ce556d851f846ca15ee3e69a7ec57f7
ACristobal <ajie.io@yahoo.com>
2026-01-04 18:11:57 -0500

    79 Common ProductList and getProducts Server Action

---
+++ components/product-list.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Product } from "@/app/generated/prisma/client";
+import { ProductCard } from "@/app/products/ProductCard";
+
+type ProductListProps = {
+  products: Product[];
+};
+
+export async function ProductList({ products }: ProductListProps) {
+  if (products.length === 0) {
+    return (
+      <div className="text-center text-muted-foreground">
+        No products found.
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+}
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,57 @@
 "use server";
 
+
+import { Prisma } from "@/app/generated/prisma/client";
 import { prisma } from "./prisma";
+
+interface GetProductsParams {
+  query?: string;
+  slug?: string;
+  sort?: string;
+  page?: number;
+  pageSize?: number;
+}
+
+export async function getProducts({
+  query,
+  slug,
+  sort,
+  page = 1,
+  pageSize = 3,
+}: GetProductsParams) {
+  const where: Prisma.ProductWhereInput = {};
+
+  if (query) {
+    where.OR = [
+      { name: { contains: query, mode: "insensitive" } },
+      { description: { contains: query, mode: "insensitive" } },
+    ];
+  }
+
+  if (slug) {
+    where.category = {
+      slug: slug,
+    };
+  }
+
+  let orderBy: Record<string, "asc" | "desc"> | undefined = undefined;
+
+  if (sort === "price-asc") {
+    orderBy = { price: "asc" };
+  } else if (sort === "price-desc") {
+    orderBy = { price: "desc" };
+  }
+
+  const skip = pageSize ? (page - 1) * pageSize : undefined;
+  const take = pageSize;
+
+  return await prisma.product.findMany({
+    where,
+    orderBy,
+    skip,
+    take,
+  });
+}
 
 export async function getProductBySlug(slug :string) {
     const product = await prisma.product.findUnique({
