dd56530958982b9addce980a0306716b30c6a30a
ACristobal <ajie.io@yahoo.com>
2026-01-09 21:51:01 -0500

    84 Finding the Right Cart: Reading cookies with the getCart Utility

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,6 +4,7 @@
 import { Prisma } from "@/app/generated/prisma/client";
 import { prisma } from "./prisma";
 import { ShoppingCart } from 'lucide-react';
+import { cookies } from 'next/headers';
 
 export interface GetProductsParams {
   query?: string;
@@ -75,5 +76,35 @@ export type CartWithProducts = Prisma.CartGetPayload<{
 
 export type ShoppingCart = CartWithProducts & {
     size: number;
-    totalPrice: number;
+    subtotal: number;
 };
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const cartId = (await cookies()).get("cartId")?.value;
+
+  const cart = cartId
+    ? await prisma.cart.findUnique({
+        where: { id: cartId },
+        include: {
+          items: {
+            include: {
+              product: true,
+            },
+          },
+        },
+      })
+    : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.length,
+    subtotal: cart.items.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    ),
+  };
+}
