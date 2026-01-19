6b25e0e5fb8707508d0d9abf06453f83e9c5ce53
ACristobal <ajie.io@yahoo.com>
2026-01-18 19:57:22 -0500

    95 Server Action: setProductQuantity

--- components/cart-entry.tsx
+++ components/cart-entry.tsx
@@ -11,7 +11,7 @@ export default function CartEntry({ cartItem }: CartEntryProps) {
   return (
     <li className="border-b border-muted flex py-4 justify-between">
       <div className="flex space-x-4">
-        <div className="overflow-hidden rounded-md border border-muted w-24 h-24">
+        <div className="overflow-hidden rounded-md border border-muted w-16 h-16">
           {cartItem.product.image && (
             <Image
               className="h-full w-full object-cover"
@@ -23,19 +23,19 @@ export default function CartEntry({ cartItem }: CartEntryProps) {
           )}
         </div>
         <div className="flex flex-col">
-          <div className="text-lg font-medium">{cartItem.product.name}</div>
+          <div className="font-medium">{cartItem.product.name}</div>
         </div>
       </div>
 
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col justify-between items-end gap-2">
         <p className="font-medium">{formatPrice(cartItem.product.price)}</p>
 
         <div className="flex items-center border border-muted rounded-full">
-          <Button variant="ghost">
+          <Button variant="ghost" className="rounded-l-full">
             <Minus className="h-4 w-4" />
           </Button>
           <p className="w-6 text-center">{cartItem.quantity}</p>
-          <Button variant="ghost">
+          <Button variant="ghost" className="rounded-r-full">
             <Plus className="h-4 w-4" />
           </Button>
         </div>
--- lib/actions.ts
+++ lib/actions.ts
@@ -172,3 +172,42 @@ export async function addToCart(productId: string, quantity: number = 1) {
 
   revalidateTag(`cart-${cart.id}`);
 }
+
+export async function setProductQuantity(productId: string, quantity: number) {
+  if (quantity < 0) {
+    throw new Error("Quantity must be at least 0");
+  }
+
+  const cart = await findCartFromCookie();
+
+  if (!cart) {
+    throw new Error("Cart not found");
+  }
+
+  // TODO: Make sure the product inventory is not exceeded
+
+  try {
+    if (quantity === 0) {
+      await prisma.cartItem.deleteMany({
+        where: {
+          cartId: cart.id,
+          productId,
+        },
+      });
+      revalidateTag(`cart-${cart.id}`);
+    } else {
+      await prisma.cartItem.updateMany({
+        where: {
+          cartId: cart.id,
+          productId,
+        },
+        data: {
+          quantity,
+        },
+      });
+    }
+  } catch (error) {
+    console.error("Error updating cart item quantity:", error);
+    throw new Error("Failed to update cart item quantity");
+  }
+}
