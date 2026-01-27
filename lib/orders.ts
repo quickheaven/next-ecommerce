445b4ec5d809b553a171684af22d286c9f86befd
ACristobal <ajie.io@yahoo.com>
2026-01-26 22:13:53 -0500

    104 createOrder Server Action

--- lib/orders.ts
+++ lib/orders.ts
@@ -1,12 +1,58 @@
 "use server";
 
+import { cookies } from "next/headers";
 import { getCart } from "./actions";
+import { prisma } from "./prisma";
 
 export async function createOrder() {
   const cart = await getCart();
 
   if (!cart || cart.items.length === 0) {
     throw new Error("Cart is empty");
+  }
+
+  try {
+    const order = await prisma.$transaction(async (tx) => {
+      const total = cart.subtotal;
+
+      const newOrder = await tx.order.create({
+        data: {
+          total,
+        },
+      });
+
+      const orderItems = cart.items.map((item) => ({
+        productId: item.product.id,
+        quantity: item.quantity,
+        orderId: newOrder.id,
+        price: item.product.price,
+      }));
+
+      await tx.orderItem.createMany({
+        data: orderItems,
+      });
+
+      await tx.cartItem.deleteMany({
+        where: {
+          cartId: cart.id,
+        },
+      });
+
+      await tx.cart.delete({
+        where: {
+          id: cart.id,
+        },
+      });
+
+      return newOrder;
+    });
+
+    (await cookies()).delete("cartId");
+
+    return order;
+  } catch (error) {
+    console.error("Error creating order:", error);
+    throw new Error("Failed to create order");
   }
 
   // TODO:
