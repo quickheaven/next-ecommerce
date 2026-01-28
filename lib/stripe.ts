2a4b68d9dfcd1dc1781cffb4a2bf1420f520e197
ACristobal <ajie.io@yahoo.com>
2026-01-27 19:34:27 -0500

    105 Stripe Checkout Session - Getting Started

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@/app/generated/prisma/client";
 import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_KEY) {
@@ -8,3 +9,15 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2025-08-27.basil",
   typescript: true,
 });
+
+export type OrderWithItemsAndProduct = Prisma.OrderGetPayload<{
+  include: {
+    items: {
+      include: {
+        product: true;
+      };
+    };
+  };
+}>;
+
+export async function createCheckoutSession(order: OrderWithItemsAndProduct) {}
