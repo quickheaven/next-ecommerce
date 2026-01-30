ba194549e4a5d4819a7bfb64f6c5e089c7dd932b
ACristobal <ajie.io@yahoo.com>
2026-01-29 20:16:35 -0500

    106 Implemented createCheckoutSession

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -20,5 +20,46 @@ export type OrderWithItemsAndProduct = Prisma.OrderGetPayload<{
   };
 }>;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function createCheckoutSession(order: OrderWithItemsAndProduct) {}
+export async function createCheckoutSession(order: OrderWithItemsAndProduct) {
+  if (!order.items || order.items.length === 0) {
+    throw new Error("Order has no items");
+  }
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
+    order.items.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.product.name,
+            description: item.product.description ?? "",
+            images: [item.product.image ?? ""],
+          },
+          unit_amount: item.product.price * 100,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+  const successUrl = `${process.env.NEXT_PUBLIC_URL}/order/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_PUBLIC_URL}/cart?cancel=true`;
+
+  try {
+    // Initialize Stripe Checkout session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items,
+      success_url: successUrl,
+      cancel_url: cancelUrl,
+      metadata: {
+        orderId: order.id.toString(),
+      },
+    });
+
+    return { sessionId: session.id, sessionUrl: session.url };
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+    throw new Error("Failed to create checkout session");
+  }
+}
