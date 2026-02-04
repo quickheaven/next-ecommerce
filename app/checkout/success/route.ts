b2738d5250ce82d5c8ac93b7ad70b1a9503d67e0
ACristobal <ajie.io@yahoo.com>
2026-02-03 21:58:52 -0500

    114 Payment Success Route Handler

---
+++ app/checkout/success/route.ts
@@ -0,0 +1,47 @@
+import { prisma } from "@/lib/prisma";
+import { stripe } from "@/lib/stripe";
+import { notFound, redirect } from "next/navigation";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const sessionId = searchParams.get("session_id");
+
+  if (!sessionId) {
+    notFound();
+  }
+
+  try {
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    const orderId = session.metadata?.orderId;
+
+    if (!orderId) {
+      notFound();
+    }
+
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+        stripeSessionId: sessionId,
+      },
+    });
+
+    if (!order) {
+      notFound();
+    }
+
+    if (order.status === "pending_payment") {
+      await prisma.order.update({
+        where: { id: order.id },
+        data: {
+          status: "payment_processed",
+        },
+      });
+    }
+
+    return redirect("/");
+  } catch (error) {
+    console.error("Error retrieving Stripe session:", error);
+    notFound();
+  }
+}
