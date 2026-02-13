a7a6a365afa9e602bc5ba00a1275b49d3b7e06d3
ACristobal <ajie.io@yahoo.com>
2026-02-12 23:53:35 -0500

    124 Hashing Passwords

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,18 @@
 import NextAuth from "next-auth";
+import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [],
 });
+
+export async function hashPassword(password: string) {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+}
+
+export async function comparePasswords(
+  password: string,
+  hashedPassword: string
+) {
+  return await bcrypt.compare(password, hashedPassword);
+}
